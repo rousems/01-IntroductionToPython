0e90fd497ebcdf9c3e149da1e13dfa70a4895acc
rousems <rousems@rose-hulman.edu>
2018-08-30 09:32:24 -0400

    done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,12 +2,14 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Matthew Rouse.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow
 
 ###############################################################################
 # TODO: 2.
@@ -27,3 +29,23 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+chad = rg.SimpleTurtle('turtle')
+chad.pen = rg.Pen('midnight blue', 5)
+chad.speed = 5
+
+for k in range(5):
+
+    chad.draw_circle(60)
+    chad.right(72)
+
+lela = rg.SimpleTurtle('turtle')
+lela.pen = rg.Pen('red', 6)
+lela.speed = 6
+
+for l in range(5):
+
+    lela.draw_square(70)
+    lela.left(72)
+    
+window.close_on_mouse_click()
